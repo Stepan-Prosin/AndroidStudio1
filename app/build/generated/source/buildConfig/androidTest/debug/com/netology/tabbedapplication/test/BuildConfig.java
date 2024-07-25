188f9140bca149cf6aa9f7e37aee2e5141de3e28
stepan TeSt <stepan.prosin@mail.ru>
2024-07-25 19:38:50 +0300

    initial commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/netology/tabbedapplication/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netology.tabbedapplication.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netology.tabbedapplication.test";
+  public static final String BUILD_TYPE = "debug";
+}
